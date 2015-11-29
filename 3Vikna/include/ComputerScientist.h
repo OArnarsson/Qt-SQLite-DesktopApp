d10f2807b9598f60ecfc52dfd65f6aed124ffda3
OrriA <orri15@ru.is>
2015-11-29 23:23:28 +0000

    Added in a Find function.

--- 3Vikna/include/ComputerScientist.h
+++ 3Vikna/include/ComputerScientist.h
@@ -1,6 +1,8 @@
 #ifndef COMPUTERSCIENTIST_H
 #define COMPUTERSCIENTIST_H
 #include <string>
+#include <set>
+#include <vector>
 #include <fstream>
 
 using namespace std;
@@ -9,17 +11,21 @@ class ComputerScientist
 {
     public:
         string ToString(bool LastNameFirst) const;
-        string const OrderedName();
+        string OrderedName() const;
         ComputerScientist();
         ComputerScientist(string first, string middle, string last, string gen, int born, int died);
         ComputerScientist(string first, string middle, string last, string gen, int born);
         ComputerScientist(string first, string last, string gen, int born, int died);
         ComputerScientist(string first, string last, string gen, int born);
         friend ostream& operator <<(ostream& outs, const ComputerScientist myScientist);
+        string getFirst();
+        string getMiddle();
+        string getLast();
         bool operator<(ComputerScientist other) const
             {
                 return firstName + " "+ middleName +  " " + lastName < other.OrderedName();
             }
+        vector <ComputerScientist> Find(string mySearch, vector <ComputerScientist> myVector);
 
 
     protected:
